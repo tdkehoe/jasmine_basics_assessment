96fae6ac9126e0c06d3748dd767361e1e041b447
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-06-04 15:42:02 -0600

    Added averageGrades, fixed lint stuff

--- grader.js
+++ grader.js
@@ -1,11 +1,10 @@
 module.exports = {
-  letterGrader: function letterGrader(grade) {
+  letterGrader: function letterGrader (grade) {
     switch (true) {
 /*
-"switch (grade)" doesn't work. That syntax only works for single answers
-
-strings because
-the cases evaluate to "grade". I.e., the first case would evaluate to
+"switch (grade)" doesn't work. That syntax only works for single expression,
+e.g., strings or numbers. Logical expressions such as "(90 <= grade && grade <= 100)"
+evaluate to true or false. E.g., the first case would evaluate to
 "(100 < grade) === grade". In constrast, with strings you could write
 
 switch (fruit) {
@@ -15,42 +14,36 @@ switch (fruit) {
     return 'chimpanzee';
 
 The user enters 'banana' and the second case evaluates to "'banana' === 'banana'".
-With numbers (or booleans?) you must use "switch (true)" or "switch (false)".
 */
 
-//The following case doesn't fix the problem with 'true' because 'true' evaluates as 0.
+// The following case doesn't fix the problem with 'true' because 'true' evaluates as 0.
       // case (grade === true):
       //  return 'Not a number from 0 to 100';
       //  break
 
       case (100 < grade):
-        return 'Too awesome!';
-        break
+        return 'Not a number from 0 to 100';
       case (90 <= grade && grade <= 100):
         return 'Awesome!';
-        break
       case (80 <= grade && grade <= 89):
         return 'B';
-        break
       case (70 <= grade && grade <= 79):
         return 'C';
-        break
       case (60 <= grade && grade <= 69):
         return 'D';
-        break
       case (2 <= grade && grade <= 59):
         return 'F';
-        break
       case (1 === grade): //Necessary because 'true' evaluates to 1
         return 'F';
-        break
       case (0 === grade): //Necessary because 'false' and 'null' evaluate to 0
         return 'F';
-        break
       default:
-      return 'Not a number from 0 to 100'
+      return 'Not a number from 0 to 100';
     }
   },
+
+  //An if-else statement has the same issues with true, false, and null inputs.
+  //Chrome Dev Tools doesn't have problems with true, false, and null.
 
   letterGraderIf: function letterGraderIf(grade) {
 
@@ -86,50 +79,28 @@ With numbers (or booleans?) you must use "switch (true)" or "switch (false)".
       else {
         return 'Not a number from 0 to 100';
       }
+  },
+
+  averageScore: function(grades1) {
+    var sum = 0;
+    grades1.forEach(function(el) {
+      sum += el;
+    });
+    return (sum / grades1.length);
   }
 
+/*
+  medianScore: function medianScore(grades) {
 
-};
+    grades.sort( function(a,b) {return a - b;} );
 
+    var half = Math.floor(grades.length/2);
 
-
-/* This works in Chrome Dev Tools
-var letterGraderIf = function letterGraderIf(grade) {
-
-  if (grade === true) {
-    return 'True!';
+    if(grades.length % 2)
+        return grades[half];
+    else
+        return (grades[half-1] + grades[half]) / 2.0;
   }
-
-  else if (grade === false) {
-    return 'False!';
-  }
-
-  else if (grade === null) {
-    return 'Null!';
-  }
-
-    else if (100 < grade) {
-      return 'Not a number from 0 to 100';
-    }
-    else if (90 <= grade && grade <= 100) {
-      return 'Awesome!';
-    }
-    else if (80 <= grade && grade <= 89) {
-      return 'B';
-    }
-    else if (70 <= grade && grade <= 79) {
-      return 'C';
-    }
-    else if (60 <= grade && grade <= 69) {
-      return 'D';
-    }
-    else if (0 <= grade && grade <= 59) {
-      return 'F';
-    }
-    else {
-      return 'Not a number from 0 to 100';
-    }
-};
-
-console.log(letterGraderIf("Fred"))
 */
+
+};
--- spec.js
+++ spec.js
@@ -2,7 +2,7 @@ var code = require('./grader.js');
 
 describe('Test letterGrader', function() {
   it('Translates numerical scores to letter grades', function() {
-    expect(code.letterGrader(101)).toEqual('Too awesome!');
+    expect(code.letterGrader(101)).toEqual('Not a number from 0 to 100');
     expect(code.letterGrader(98)).toEqual('Awesome!');
     expect(code.letterGrader(85)).toEqual('B');
     expect(code.letterGrader(76)).toEqual('C');
@@ -15,21 +15,39 @@ describe('Test letterGrader', function() {
     expect(code.letterGrader(null)).toEqual('Not a number from 0 to 100');
     expect(code.letterGrader(undefined)).toEqual('Not a number from 0 to 100');
   });
-}),
+});
 
 describe('Test letterGraderIf', function() {
   it('Translates numerical scores to letter grades', function() {
-    expect(code.letterGrader(101)).toEqual('Too awesome!');
-    expect(code.letterGrader(98)).toEqual('Awesome!');
-    expect(code.letterGrader(85)).toEqual('B');
-    expect(code.letterGrader(76)).toEqual('C');
-    expect(code.letterGrader(70)).toEqual('C');
-    expect(code.letterGrader(62)).toEqual('D');
-    expect(code.letterGrader(37)).toEqual('F');
-    expect(code.letterGrader('Barney')).toEqual('Not a number from 0 to 100');
-    // expect(code.letterGrader(true)).toEqual('Not a number from 0 to 100');
-    // expect(code.letterGrader(false)).toEqual('Not a number from 0 to 100');
-    // expect(code.letterGrader(null)).toEqual('Not a number from 0 to 100');
-    expect(code.letterGrader(undefined)).toEqual('Not a number from 0 to 100');
+    expect(code.letterGraderIf(101)).toEqual('Not a number from 0 to 100');
+    expect(code.letterGraderIf(98)).toEqual('Awesome!');
+    expect(code.letterGraderIf(85)).toEqual('B');
+    expect(code.letterGraderIf(76)).toEqual('C');
+    expect(code.letterGraderIf(70)).toEqual('C');
+    expect(code.letterGraderIf(62)).toEqual('D');
+    expect(code.letterGraderIf(37)).toEqual('F');
+    expect(code.letterGraderIf('Barney')).toEqual('Not a number from 0 to 100');
+    // expect(code.letterGraderIf(true)).toEqual('Not a number from 0 to 100');
+    // expect(code.letterGraderIf(false)).toEqual('Not a number from 0 to 100');
+    // expect(code.letterGraderIf(null)).toEqual('Not a number from 0 to 100');
+    expect(code.letterGraderIf(undefined)).toEqual('Not a number from 0 to 100');
   });
 });
+
+var grades1 = [90, 95, 87, 60];
+
+describe('Test averageScore', function() {
+  it('Traverses an array of numbers and returns the average', function() {
+    expect(code.averageScore(grades1)).toEqual(83);
+  });
+});
+
+/*
+var grades2 = [52,80,80,86,94];
+
+describe('Test medianScore', function() {
+  it('Traverses an array of numbers and returns the median', function() {
+    expect(code.medianScore(grades2)).toEqual(80);
+  });
+});
+*/
